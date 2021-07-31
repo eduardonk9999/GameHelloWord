546b7eb7d432315cdebac1a212e7f3e65b37bb3e
eduardonk9999 <thiago.novak7@gmail.com>
2021-07-31 11:15:59 -0300

    Add, script da estrela

--- assets/Scripts/Game.js
+++ assets/Scripts/Game.js
@@ -1,38 +1,49 @@
-// Learn cc.Class:
-//  - https://docs.cocos.com/creator/manual/en/scripting/class.html
-// Learn Attribute:
-//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
-
 cc.Class({
-    extends: cc.Component,
+  extends: cc.Component,
 
-    properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
+  properties: {
+    starPrefab: {
+      default: null,
+      type: cc.Prefab
     },
+    maxStarDuration: 0,
+    minStarDuration: 0,
 
-    // LIFE-CYCLE CALLBACKS:
-
-    // onLoad () {},
-
-    start () {
-
+    ground: {
+      default: null,
+      type: cc.Node
     },
+    player: {
+      default: null,
+      type: cc.Node
+    }
+      
+  },
 
-    // update (dt) {},
+   
+    
+
+
+  onLoad () {
+    //this.groundY = this.ground.y + this.ground.height/2; // "this.ground.top" may also work
+    this.spawnNewStar();
+  },
+
+  spawnNewStar: function() {
+    var newStar = cc.instantiate(this.starPrefab);
+    this.node.addChild(newStar);
+    newStar.setPosition(this.getNewStarPosition());
+  },
+  getNewStarPosition: function () {
+    var randX = 0;
+    var randY = this.groundY + Math.random() * this.player.getComponent('Player').jumpHeight + 50;
+    var maxX = this.node.width/2;
+    randX = (Math.random() - 0.5) * 2 * maxX;
+    return cc.v2(randX, randY);
+  },
+
+
+  //start () { },
+
+  // update (dt) {},
 });
